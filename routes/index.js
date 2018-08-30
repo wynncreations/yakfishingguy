5784af5ef8dd13da4b8b03320b5e5e47e3860bee
robert wynn <wynncreations@gmail.com>
2018-08-30 16:41:14 -0500

    committing cahnges from merge that went wonky

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-var express = require("express");
-var router = express.Router();
-var passport = require("passport");
-var User = require("../models/user");
-var request = require("request");
-
-router.get("/",function(req,res){
-    //Get user id
-    
-    //get media url with id.
-
-    //send media url to the landing page.
-    res.render("landing");
-});
-
-router.get("/tos", function (req, res) {
-    res.render("tos");
-});
-
-router.get("/privacy", function (req, res) {
-    res.render("privacy");
-});
-
-
-
-=======
 const express   = require('express');
 const router    = express.Router();
 
@@ -32,5 +5,4 @@ router.get('/',(req,res) => {
   res.render('landing')
 });
 
->>>>>>> oauth
 module.exports = router;
